f9fff782c15d5b9b14cf5a763f649212ba43370e
hiiammikezzz <105882098+hiiammikezzz@users.noreply.github.com>
2024-04-10 17:06:25 +0800

    Update networks.py
    line 314, update 		
    #vgglua = load_lua(os.path.join(model_folder, 'vgg16.t7')) 
    vgglua = torchfile.load(os.path.join(model_folder, 'vgg16.t7'))

--- src/networks.py
+++ src/networks.py
@@ -311,7 +311,8 @@ def init_vgg16(model_folder):
 		if not os.path.exists(os.path.join(model_folder, 'vgg16.t7')):
 			os.system(				# 下载vgg16.t7文件
 				'wget http://cs.stanford.edu/people/jcjohns/fast-neural-style/models/vgg16.t7 -O ' + os.path.join(model_folder, 'vgg16.t7'))
-		vgglua = load_lua(os.path.join(model_folder, 'vgg16.t7'))
+		#vgglua = load_lua(os.path.join(model_folder, 'vgg16.t7'))
+		vgglua = torchfile.load(os.path.join(model_folder, 'vgg16.t7'))
 		vgg = Vgg16()
 		for (src, dst) in zip(vgglua.parameters()[0], vgg.parameters()):
 			dst.data[:] = src
